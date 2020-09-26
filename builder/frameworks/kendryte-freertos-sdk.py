b14ef505bdc71d15e46b57f28fa6fb1138d21b1b
Neil Maitland <neil@hydeabbey.net>
2020-09-26 15:13:03 +0200

    #32: Moved crtend objects to end of linker command line

--- builder/frameworks/kendryte-freertos-sdk.py
+++ builder/frameworks/kendryte-freertos-sdk.py
@@ -70,8 +70,6 @@ env.Append(
         "-Wl,--end-group",
         join(TOOLCHAIN_DIR, "lib", "gcc", "riscv64-unknown-elf", "8.2.0", "crti.o"),
         join(TOOLCHAIN_DIR, "lib", "gcc", "riscv64-unknown-elf", "8.2.0", "crtbegin.o"),
-        join(TOOLCHAIN_DIR, "lib", "gcc", "riscv64-unknown-elf", "8.2.0", "crtend.o"),
-        join(TOOLCHAIN_DIR, "lib", "gcc", "riscv64-unknown-elf", "8.2.0", "crtn.o")
     ],
 
     CPPPATH = [
@@ -105,13 +103,20 @@ env.Append(
     ],
 
     LIBS = [
-        "c", "gcc", "m", "stdc++", "atomic"
+        "c", "gcc", "m", "stdc++", "atomic"                                                                                                                 
     ]
 
 )
 
 if not env.BoardConfig().get("build.ldscript", ""):
     env.Replace(LDSCRIPT_PATH=join(FRAMEWORK_DIR, "lds", "kendryte.ld"))
+
+env.Append(CRTEND=[
+    join(TOOLCHAIN_DIR, "lib", "gcc", "riscv64-unknown-elf", "8.2.0", "crtend.o"),
+    join(TOOLCHAIN_DIR, "lib", "gcc", "riscv64-unknown-elf", "8.2.0", "crtn.o")
+])
+
+env.Append(LINKCOM=" $CRTEND")
 
 #
 # Target: Build Core Library
